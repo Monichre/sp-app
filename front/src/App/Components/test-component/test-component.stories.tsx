2b5fc7956d8c113efede270e1b6b59a77d871d20
mikelsito <mikelsito@gmail.com>
2020-01-15 20:21:47 -0600

    Deleting accidental addition of test_component folder and files

--- front/src/App/Components/test-component/test-component.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { test-component } from './test-component';
-
-
-export default { title: 'test-component' };
-
-export const test-componentFirstStory = () => <test-component />;
--- front/src/App/Components/test-component/test-component.styles.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import styled, { css } from 'styled-components';
-
-
-// Write some styles mutha fuckah
-
-
